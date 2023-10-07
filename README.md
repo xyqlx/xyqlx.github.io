1f79478944fee922041dfdc147608a493e331319
xyqlx <saint.diana@outlook.com>
2023-10-07 17:01:44 +0800

    🎉init

---
+++ README.md
@@ -0,0 +1,7 @@
+# xyqlx-channel
+
+Personal website for xyqlx showcasing 📦projects and 🌟skills.
+
+## Under Construction
+
+This website is currently under construction. Please check back later.
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="main">
+      <h1>This is home page</h1>
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .main {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
