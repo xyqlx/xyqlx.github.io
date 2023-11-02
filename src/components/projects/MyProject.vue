bf2b07ce741c39aaa94fcd0994bc4fce92b0084a
xyqlx <saint.diana@outlook.com>
2023-11-02 10:41:16 +0800

    💄change scrollbar position in project view

--- src/components/projects/MyProject.vue
+++ src/components/projects/MyProject.vue
@@ -34,56 +34,74 @@ const { t } = useI18n();
 </i18n>
 
 <template>
-  <div class="project-container" v-if="currentProject">
-    <el-scrollbar height="calc(100vh - 160px)">
-      <div class="title-bar">
-        <h1>{{ currentProject.name }}</h1>
-        <div class="shortcuts">
-          <template v-for="(url, i) in shortcuts" :key="i">
-            <a class="tag-container" v-if="url" :href="url">
-              <el-tag class="tag" effect="plain" color="transparent">{{ tagNames[i] }}</el-tag>
-            </a>
+  <div class="scrollbar-container" v-if="currentProject">
+    <el-scrollbar view-class="scrollbar" height="calc(100vh - 160px)">
+      <div class="project-container">
+        <div class="title-bar">
+          <h1>{{ currentProject.name }}</h1>
+          <div class="shortcuts">
+            <template v-for="(url, i) in shortcuts" :key="i">
+              <a class="tag-container" v-if="url" :href="url">
+                <el-tag class="tag" effect="plain" color="transparent">{{ tagNames[i] }}</el-tag>
+              </a>
+            </template>
+          </div>
+        </div>
+        <div>
+          <slot></slot>
+          <template v-if="!defaultSlot">
+            {{ t('under construction') }}
           </template>
         </div>
-      </div>
-      <div>
-        <slot></slot>
-        <template v-if="!defaultSlot">
-          {{ t('under construction') }}
-        </template>
       </div>
     </el-scrollbar>
   </div>
 </template>
 
 <style>
+.scrollbar-container {
+  width: 100%;
+}
+
+.scrollbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
 .project-container {
   width: 60%;
 }
+
 .title-bar {
   display: flex;
   flex-direction: row;
   align-items: center;
 }
+
 .shortcuts {
   margin-top: 0.5rem;
   margin-left: 1rem;
 }
+
 a:hover {
   background-color: transparent;
 }
+
 .tag {
   color: var(--color-text);
 }
+
 .tag:hover {
   text-decoration: underline;
 }
+
 .tag-container {
   margin-left: 0.8rem;
 }
+
 @media (max-width: 1024px) {
   .project-container {
     width: 100%;
   }
-}
-</style>
+}</style>
